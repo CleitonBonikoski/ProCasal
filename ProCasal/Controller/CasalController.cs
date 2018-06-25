2d33db48931a63e8afc6d81c3cf6a05a24c19a26
Cleiton <cleitonbonikoski57@gmail.com>
2018-06-25 09:07:10 -0300

    Ajustes Referencias

--- ProCasal/Controller/CasalController.cs
+++ ProCasal/Controller/CasalController.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Model
+namespace Controller
 {
-    class CasalController
+    class CasalController 
     {
+
     }
 }
