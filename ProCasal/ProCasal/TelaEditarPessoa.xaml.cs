378b797f4f741fa5c24b82e1dfa49d7426e630c7
Cleiton <cleitonbonikoski57@gmail.com>
2018-06-29 04:09:47 -0300

    Criando Tela EditarPessoa

---
+++ ProCasal/ProCasal/TelaEditarPessoa.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProCasal
+{
+    /// <summary>
+    /// Interaction logic for TelaEditarPessoa.xaml
+    /// </summary>
+    public partial class TelaEditarPessoa : Window
+    {
+        public TelaEditarPessoa()
+        {
+            InitializeComponent();
+        }
+
+        private void EditarPessoa(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
